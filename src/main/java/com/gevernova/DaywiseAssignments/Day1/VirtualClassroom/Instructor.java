73249ad037603b6c63c97375c9f86450bc7db467
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-25 20:41:02 +0530

    [saptadipa77]:Added Instructor for Virtual Classroom

---
+++ src/main/java/com/gevernova/DaywiseAssignments/Day1/VirtualClassroom/Instructor.java
@@ -0,0 +1,10 @@
+package com.gevernova.DaywiseAssignments.Day1.VirtualClassroom;
+
+public class Instructor {
+    String id;
+    String name;
+    public Instructor(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
