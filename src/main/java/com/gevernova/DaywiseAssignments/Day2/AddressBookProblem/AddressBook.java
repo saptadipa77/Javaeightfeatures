e1e939c50d703b8f92ad2c4aba9fff1968bd29d2
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-25 18:42:48 +0530

    [saptadipa77]:Added UC2->add new Contact in Address Book for AddressBook

---
+++ src/main/java/com/gevernova/DaywiseAssignments/Day2/AddressBookProblem/AddressBook.java
@@ -0,0 +1,15 @@
+package com.gevernova.DaywiseAssignments.Day2.AddressBookProblem;
+
+import java.util.*;
+public class AddressBook {
+    private List<Contact> contactList = new ArrayList<>();
+
+
+
+
+    public void addContact(Contact contact) {
+
+        contactList.add(contact);
+    }
+
+}
