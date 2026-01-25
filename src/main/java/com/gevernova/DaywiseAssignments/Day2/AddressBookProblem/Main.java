b204a1fb898f0d38463610a2cb733377887b204c
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-25 18:36:26 +0530

    [saptadipa77]:Added initial Welcome message for AddressBook

---
+++ src/main/java/com/gevernova/DaywiseAssignments/Day2/AddressBookProblem/Main.java
@@ -0,0 +1,11 @@
+package com.gevernova.DaywiseAssignments.Day2.AddressBookProblem;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the address book");
+    }
+}
+
