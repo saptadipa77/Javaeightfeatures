63d7bfeeb39899911115f8ca98efe9d93a935c7b
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-25 20:48:35 +0530

    [saptadipa77]:Added Submission for Virtual Classroom

---
+++ src/main/java/com/gevernova/DaywiseAssignments/Day1/VirtualClassroom/Submission.java
@@ -0,0 +1,12 @@
+package com.gevernova.DaywiseAssignments.Day1.VirtualClassroom;
+
+public class Submission {
+    String studentid;
+    String content;
+    String feedback;
+    String grade;
+    public Submission(String studentid, String content) {
+        this.studentid = studentid;
+        this.content = content;
+    }
+}
