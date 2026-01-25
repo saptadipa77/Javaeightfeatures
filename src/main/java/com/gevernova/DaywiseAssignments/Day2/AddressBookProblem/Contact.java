ac0b93eb475511689f409c35e4983138cf0f413c
saptadipa77 <saptadipadatta5@gmail.com>
2026-01-25 18:40:07 +0530

    [saptadipa77]:Added UC1->create Contacts in Address Book for AddressBook

---
+++ src/main/java/com/gevernova/DaywiseAssignments/Day2/AddressBookProblem/Contact.java
@@ -0,0 +1,98 @@
+package com.gevernova.DaywiseAssignments.Day2.AddressBookProblem;
+
+public class Contact {
+    // UC 1: Ability to create a Contact with specific details [cite: 11, 12, 13]
+    private String firstName, lastName, address, city, state, email;
+    private int zip;
+    private long phoneNumber;
+
+    public Contact(String firstName, String lastName, String address, String city, String state, String email,int zip, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", email='" + email + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
+
